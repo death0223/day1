339d7a4649dbc4eaf373a5feba45f62bf9bbb58d
hongkefei <cjsswf@126.com>
2020-12-15 19:23:05 +0800

    夸奖了一下班长不会唱歌

--- app/src/main/java/com/example/day1/MainActivity.java
+++ app/src/main/java/com/example/day1/MainActivity.java
@@ -11,6 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 		//我提交的，你撒旦立刻就发发看的萨芬那边行政成本v现在看v在v
+		//班长不会唱歌，贼难听。
+		
 		
     }
 }
